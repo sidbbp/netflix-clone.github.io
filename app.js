e686e1fcb9a77d0a7127ccd970e4df952f26434d
sidbbp <89354845+sidbbp@users.noreply.github.com>
2023-06-23 10:32:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+var iframe = document.querySelector('iframe [src="SnapSave.io-Rick Astley - Never Gonna Give You Up (Official Music Video)(720p).mp4"]');
+
+iframe.contentWindow.speaker[0].muted = true
+iframe.contentWindow.speaker[1].muted = true
